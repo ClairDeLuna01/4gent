8eadf5f417cbdaacf953767a80013b0e9213e7ac
MonsieurBleu <97893210+MonsieurBleu@users.noreply.github.com>
2023-09-27 14:42:54 +0200

    updating board class

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <board.hpp>
 
 int main(void) {
-	std::cout << "Blue n'est pas un gamer pog";
+	Board board;
+	board.play(red, 1);
+	board.play(red, 1);
+	board.play(red, 1);
+	board.play(red, 1);
+	board.play(red, 1);
+	board.play(red, 1);
+	board.play(red, 1);
+	board.play(red, 6);
+	board.play(yellow, 2);
+	std::cout << board;
+
+	std::cout << "\nlegal moves :";
+	std::vector<int> test = board.getLegalMoves();
+	for(size_t i  = 0; i < test.size(); i++)
+		std::cout << test[i] << " ";
+
 	return 0;
 }
 
---
+++ src/board.cpp
@@ -0,0 +1,79 @@
+#include <board.hpp>
+#include <string.h>
+
+token Board::operator()(int x, int y) const
+{
+    return grid[x%BOARD_SIZE_X][y%BOARD_SIZE_Y];
+}
+
+bool Board::play(token color, int x)
+{
+    if(x >= BOARD_SIZE_X || grid[x][BOARD_SIZE_Y-1] != empty)
+    {
+        std::cout << "illegal move :(\n";
+        return false;
+    }
+
+    for(int i = BOARD_SIZE_Y-1; i >= 0; i--)
+    {
+        if(!i || grid[x][i-1] != empty)
+        {
+            grid[x][i] = color;
+            break;
+        }
+    }
+
+    return true;
+}
+
+const std::vector<int> Board::getLegalMoves() const
+{
+    std::vector<int> res;
+
+    for(int x = 0; x < BOARD_SIZE_X; x++)
+        if(grid[x][BOARD_SIZE_Y-1] == empty)
+            res.push_back(x);
+    
+    return res;
+}
+
+void Board::clear() const
+{
+    memset((char *)grid, 0, BOARD_SIZE_X*BOARD_SIZE_Y);
+}
+
+std::ostream& operator<<(std::ostream& os, const token t)
+{
+    char tyellow[] = "\033[93m";
+    char tred[] = "\033[31m";
+    char treset[] = "\033[0m";
+    
+    switch (t)
+    {
+        case empty: os << " "; break;
+        case red: os << tred <<"O" << treset; break;
+        case yellow: os << tyellow <<"O" << treset; break;
+        default: os << treset << "$"; break;
+    }
+
+    return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const Board& b)
+{
+    os << "\n";
+    for(int y = BOARD_SIZE_Y-1; y >= 0; y--)
+    {
+        os << "|";
+        for(int x = 0; x < BOARD_SIZE_X; x++)
+        {
+            os << b(x, y) << " ";
+        }
+
+        os << "|\n";
+    }
+
+    os << "================\n";
+
+    return os;
+}
